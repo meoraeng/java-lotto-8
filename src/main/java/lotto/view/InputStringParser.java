931a9c0956af4b3ab76fdc34f02b98a0ce75755b
SeonWu Kim <sunwoo005@naver.com>
2025-11-02 21:10:30 +0900

    feat(InputStringParser): 문자열 입력에서 정수 및 정수 콜렉션으로 파싱 구현

---
+++ src/main/java/lotto/view/InputStringParser.java
@@ -0,0 +1,46 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import lotto.global.exception.ErrorMessage;
+
+
+public class InputStringParser {
+    private static final Pattern COMMA_SEPARATOR_REGEX = Pattern.compile("\\s*,\\s*");
+
+    private InputStringParser() {}
+
+    public static List<Integer> stringsToIntegers(String input) {
+        validateNonBlank(input);
+        String[] tokens = split(input);
+        List<Integer> numbers = new ArrayList<>(tokens.length);
+        for (String token : tokens) {
+            numbers.add(validateNumberFormat(token));
+        }
+        return numbers;
+    }
+
+    public static int stringToInteger(String input) {
+        validateNonBlank(input);
+        return validateNumberFormat(input.trim());
+    }
+
+    private static String[] split(String input) {
+        return COMMA_SEPARATOR_REGEX.split(input);
+    }
+
+    private static int validateNumberFormat(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_INPUT_ERROR.getMessage());
+        }
+    }
+
+    private static void validateNonBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT_ERROR.getMessage());
+        }
+    }
+}
